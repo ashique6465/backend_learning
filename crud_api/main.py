65021722be7adc2d83889fcbec235a7bc4c794ee
Md Ashique Ali <143936710+ashique6465@users.noreply.github.com>
2026-01-10 18:19:09 +0000

    Create a full crud api with postgresql

---
+++ crud_api/main.py
@@ -0,0 +1,42 @@
+from fastapi import FastAPI, Depends, HTTPException
+
+from sqlalchemy.orm import Session
+
+from database import Base, engine, get_db
+import models, schemas, crud 
+
+Base.metadata.create_all(bind=engine)
+
+app =FastAPI()
+
+@app.post("/users/", response_model=schemas.UserResponse)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    return crud.create_user(db, user)
+
+
+@app.get("/users/", response_model=list[schemas.UserResponse])
+def read_users(db:Session = Depends(get_db)):
+    return crud.get_users(db)
+
+
+@app.get("/users/{user_id}", response_model=schemas.UserResponse)
+def read_user(user_id: int, db: Session = Depends(get_db)):
+    user = crud.get_user(db, user_id)
+    if not user:
+        raise HTTPException(status_code=404, detail = "User not found")
+    return user 
+
+@app.put("/users/{user_id}", response_model=schemas.UserResponse)
+def update_user(user_id: int, user: schemas.UserUpdate, db: Session = Depends(get_db)):
+    updated_user = crud.update_user(db, user_id, user)
+    if not updated_user:
+        raise HTTPException(status_code=404, detail= "Use not found")
+    return updated_user
+
+@app.delete("/users/{user_id}")
+def delete_user(user_id: int, db: Session = Depends(get_db)):
+    deleted = crud.delete_user(db, user_id)
+    if not deleted:
+        raise HTTPException(status_code=404, detail="User not found")
+    return {"message": "User deleted "}
+
