77299ff47a845b8aa9d2a5ebabff0162de8aeeaf
Md Ashique Ali <143936710+ashique6465@users.noreply.github.com>
2026-01-20 11:38:17 +0000

    restructured the folder

--- rate_limiting/rate_limiter.py
+++ rate_limiting/rate_limiter.py
@@ -12,16 +12,20 @@ redis_client = redis.Redis(
 
 
 RATE_LIMIT = 5 
-WINDOW_SIZE = 10 
+WINDOW_SIZE = 30
 
 
 def rate_limiter(request: Request):
     print("RATE LIMITER CALLED")
+
     client_ip = request.client.host
+
     redis_key = f"rate_limit:{client_ip}"
     print("Redis key:", redis_key)
+
     current_count = redis_client.incr(redis_key)
     print("Current count:", current_count)
+
     if current_count == 1 :
         redis_client.expire(redis_key, WINDOW_SIZE)
 
