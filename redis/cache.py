2827f0b4987028d82c29274dcfb74d757077db9d
Md Ashique Ali <143936710+ashique6465@users.noreply.github.com>
2026-02-07 18:43:32 +0000

    redis invalidate

--- redis/cache.py
+++ redis/cache.py
@@ -5,6 +5,7 @@ from redis_client import redis_client
 PRODUCTS_CACHE_KEY = "products:list:v1"
 CACHE_TTL = 60 
 
+#Simulating a database with an in-memory list
 def get_products_cache():
     cached_data = redis_client.get(PRODUCTS_CACHE_KEY)
 
@@ -15,6 +16,7 @@ def get_products_cache():
     print("REDIS CACHE MISS")
     return None
 
+#Functions to interact with the fake database and manage cache
 def set_products_cache(products: list):
     redis_client.setex(
         PRODUCTS_CACHE_KEY,
@@ -22,6 +24,8 @@ def set_products_cache(products: list):
         json.dumps(products)
     )
 
+
+#When products are added or update, we need to invalidate the cache to ensure fresh data is fetched
 def invalidate_products_cache():
     redis_client.delete(PRODUCTS_CACHE_KEY)
     print("Cache Invalidated")
