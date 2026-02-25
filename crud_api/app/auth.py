ee3bc4b7145ee79c9756c75747046614a1cfc7ed
Md Ashique Ali <143936710+ashique6465@users.noreply.github.com>
2026-02-25 17:11:11 +0000

    route add

--- crud_api/app/auth.py
+++ crud_api/app/auth.py
@@ -7,11 +7,13 @@ from .jwt import create_access_token
 
 router = APIRouter(prefix="/auth", tags=["auth"])
 
+#register route
 @router.post("/register", response_model=schemas.UserResponse)
 def register(user: schemas.UserCreate, db: Session = Depends(get_db)):
     return crud.create_user(db, user)
 
 
+#login route
 @router.post("/login", response_model=schemas.Token)
 def login(user: schemas.UserLogin, db: Session = Depends(get_db)):
     authenticate_user = crud.authenticate_user(
