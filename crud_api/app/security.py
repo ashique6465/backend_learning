6715edffbd3ae7b398e63f5824a8d4dd5d6996c9
Md Ashique Ali <143936710+ashique6465@users.noreply.github.com>
2026-03-05 16:18:35 +0000

    verify and hash

--- crud_api/app/security.py
+++ crud_api/app/security.py
@@ -1,5 +1,7 @@
 import bcrypt
 
+
+#hashing the password 
 def hash_password(password: str) -> str:
     password_bytes = password.encode("utf-8")
     salt = bcrypt.gensalt()
@@ -7,6 +9,7 @@ def hash_password(password: str) -> str:
     return hashed.decode("utf-8")
 
 
+#verifying the password
 def verify_password(password: str, hashed_password: str) -> bool:
     return bcrypt.checkpw(
         password.encode("utf-8"),
